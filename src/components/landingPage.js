638fe40878547ea90291405bc832ce139043067b
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-02 21:37:16 -0700

    services custom software dev section

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -9,6 +9,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 import animationData from "../animations/landinganimation/data.js";
+import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -35,13 +36,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "1em",
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: "none",
-    borderRadius: 50,
-    fontFamily: "Roboto",
-    fontWeight: "bold",
+    ...theme.typography.learnButton,
     fontSize: "0.9em",
     height: 45,
     width: 145,
@@ -59,6 +54,19 @@ const useStyles = makeStyles((theme) => ({
     minWidth: "21.5em",
     marginLeft: "1em",
     [theme.breakpoints.down("xs")]: { marginLeft: 0 },
+  },
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange,
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+  },
+  subtitle: {
+    marginBottom: "1em",
   },
 }));
 
@@ -78,6 +86,7 @@ const LandingPage = () => {
   return (
     <Grid container direction="column" className={classes.mainContainer}>
       <Grid item>
+        {/*----- Hero Block ------ */}
         <Grid container justify="flex-end" alignItems="center" direction="row">
           <Grid sm item className={classes.heroTextContainer}>
             <Typography variant="h2" align="center">
@@ -108,6 +117,32 @@ const LandingPage = () => {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie options={defaultOption} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/* ------- Services -------- */}
+        <Grid container direction="row">
+          <Grid item>
+            <Typography variant="h4">Custom Software Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Save Enery. Save Time. Save Money.
+            </Typography>
+            <Typography variant="subtitle1">
+              Complete digital Solution, from investigation to{" "}
+              <span className={classes.specialText}>celebration</span>
+            </Typography>
+            <Button variant="outlined" className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More </span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img src={customSoftwareIcon} alt="Customer Software Icon" />
           </Grid>
         </Grid>
       </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,18 +2,19 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 const arcBlue = "#0B72B9";
 const arcOrange = "#FFBA60";
+const arcGray = "#868686";
 
 export default createMuiTheme({
   palette: {
     common: {
-      blue: `${arcBlue}`,
-      orange: `${arcOrange}`,
+      blue: arcBlue,
+      orange: arcOrange,
     },
     primary: {
-      main: `${arcBlue}`,
+      main: arcBlue,
     },
     secondary: {
-      main: `${arcOrange}`,
+      main: arcOrange,
     },
   },
   typography: {
@@ -33,8 +34,28 @@ export default createMuiTheme({
       fontFamily: "Raleway",
       fontWeight: 700,
       fontSize: "2.2rem",
-      color: `${arcBlue}`,
+      color: arcBlue,
       lineHeight: 1.5,
+    },
+    h4: {
+      fontFamily: "Raleway",
+      fontSize: "1.75rem",
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    subtitle1: {
+      fontWeight: 300,
+      color: arcGray,
+      fontSize: "1.25rem",
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: "none",
+      borderRadius: 50,
+      fontFamily: "Roboto",
+      fontWeight: "bold",
     },
   },
 });
