e9d5e30f74c973423a0f4af26a6b3933a6bbb778
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-02 21:57:49 -0700

    custom software dev section make responsive

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -7,6 +7,7 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import animationData from "../animations/landinganimation/data.js";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
@@ -64,15 +65,31 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "0.7rem",
     height: 35,
     padding: 5,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
+    },
   },
   subtitle: {
     marginBottom: "1em",
+  },
+  csIcon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25,
+    },
   },
 }));
 
 const LandingPage = () => {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
   const defaultOption = {
     loop: true,
@@ -122,8 +139,19 @@ const LandingPage = () => {
       </Grid>
       <Grid item>
         {/* ------- Services -------- */}
-        <Grid container direction="row">
-          <Grid item>
+        <Grid
+          container
+          direction="row"
+          justify={matchesSM ? "center" : undefined}
+          className={classes.serviceContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : "left",
+            }}
+          >
             <Typography variant="h4">Custom Software Development</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>
               Save Enery. Save Time. Save Money.
@@ -142,7 +170,11 @@ const LandingPage = () => {
             </Button>
           </Grid>
           <Grid item>
-            <img src={customSoftwareIcon} alt="Customer Software Icon" />
+            <img
+              className={classes.csIcon}
+              src={customSoftwareIcon}
+              alt="Customer Software Icon"
+            />
           </Grid>
         </Grid>
       </Grid>
