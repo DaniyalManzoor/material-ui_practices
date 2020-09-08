75d7f1e4ad9659cba0ea04c73c4bf1add6f3ffd7
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-08 07:14:50 -0700

    add services section into services page

--- src/components/services.js
+++ src/components/services.js
@@ -1,14 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ButtonArrow from "./ui/buttonArrow";
+
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-const useStyle = makeStyles((theme) => ({}));
+import mobileAppIcon from "../assets/mobileIcon.svg";
+import websitesIcon from "../assets/websiteIcon.svg";
+import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 
-const Services = () => {
+const useStyle = makeStyles((theme) => ({
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange,
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
+    },
+  },
+  subtitle: {
+    marginBottom: "1em",
+  },
+  csIcon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: "10em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25,
+    },
+  },
+}));
+
+const Services = (props) => {
   const classes = useStyle();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
@@ -16,6 +51,73 @@ const Services = () => {
   return (
     <>
       <Grid container direction="column">
+        <Grid
+          item
+          style={{
+            marginLeft: matchesSM ? 0 : "5em",
+            marginTop: matchesSM ? "1em" : "2em",
+          }}
+        >
+          <Typography
+            align={matchesSM ? "center" : undefined}
+            variant="h2"
+            gutterBottom
+          >
+            Services
+          </Typography>
+        </Grid>
+        <Grid item>
+          {/* ------- Mobile App Developement -------- */}
+          <Grid
+            container
+            direction="row"
+            justify={matchesSM ? "center" : "flex-end"}
+            className={classes.serviceContainer}
+            style={{ marginTop: matchesSM ? "1em" : "5em" }}
+          >
+            <Grid
+              item
+              style={{
+                textAlign: matchesSM ? "center" : "left",
+                width: matchesSM ? undefined : "30em",
+              }}
+            >
+              <Typography variant="h4">iOS/Android app Development</Typography>
+              <Typography variant="subtitle1" className={classes.subtitle}>
+                Extend Functionality. Extend Access. Increase Engagement.
+              </Typography>
+              <Typography variant="subtitle1">
+                Integrate Your Web Experience or create a standalone app{" "}
+                {matchesSM ? null : <br />} either mobile platform.
+              </Typography>
+              <Button
+                component={Link}
+                to="/mobileapps"
+                variant="outlined"
+                className={classes.learnButton}
+                onClick={() => {
+                  props.setValue(1);
+                  props.setSelectedIndex(2);
+                }}
+              >
+                <span style={{ marginRight: 10 }}>Learn More </span>
+                <ButtonArrow
+                  width={10}
+                  height={10}
+                  fill={theme.palette.common.blue}
+                />
+              </Button>
+            </Grid>
+            <Grid item style={{ marginRight: matchesSM ? 0 : "5em" }}>
+              <img
+                className={classes.csIcon}
+                src={mobileAppIcon}
+                alt="Mobile App Icon"
+                width="250em"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
         <Grid item>
           {/* ------- Custom software Services -------- */}
           <Grid
@@ -67,7 +169,7 @@ const Services = () => {
           </Grid>
         </Grid>
         <Grid item>
-          {/* ------- Mobile App Developement -------- */}
+          {/* ------- Websites Services -------- */}
           <Grid
             container
             direction="row"
@@ -78,56 +180,7 @@ const Services = () => {
               item
               style={{
                 textAlign: matchesSM ? "center" : "left",
-              }}
-            >
-              <Typography variant="h4">iOS/Android app Development</Typography>
-              <Typography variant="subtitle1" className={classes.subtitle}>
-                Extend Functionality. Extend Access. Increase Engagement.
-              </Typography>
-              <Typography variant="subtitle1">
-                Integrate Your Web Experience or create a standalone app{" "}
-                {matchesSM ? null : <br />} either mobile platform.
-              </Typography>
-              <Button
-                component={Link}
-                to="/mobileapps"
-                variant="outlined"
-                className={classes.learnButton}
-                onClick={() => {
-                  props.setValue(1);
-                  props.setSelectedIndex(2);
-                }}
-              >
-                <span style={{ marginRight: 10 }}>Learn More </span>
-                <ButtonArrow
-                  width={10}
-                  height={10}
-                  fill={theme.palette.common.blue}
-                />
-              </Button>
-            </Grid>
-            <Grid item style={{ marginRight: matchesSM ? 0 : "5em" }}>
-              <img
-                className={classes.csIcon}
-                src={mobileAppIcon}
-                alt="Mobile App Icon"
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          {/* ------- Websites Services -------- */}
-          <Grid
-            container
-            direction="row"
-            justify={matchesSM ? "center" : undefined}
-            className={classes.serviceContainer}
-          >
-            <Grid
-              item
-              style={{
-                marginLeft: matchesSM ? 0 : "5em",
-                textAlign: matchesSM ? "center" : "left",
+                width: matchesSM ? undefined : "30em",
               }}
             >
               <Typography variant="h4">Website Developement</Typography>
@@ -155,11 +208,17 @@ const Services = () => {
                 />
               </Button>
             </Grid>
-            <Grid item>
+            <Grid
+              item
+              style={{
+                marginRight: matchesSM ? 0 : "5em",
+              }}
+            >
               <img
                 className={classes.csIcon}
                 src={websitesIcon}
                 alt="Website Icon"
+                width="250em"
               />
             </Grid>
           </Grid>
